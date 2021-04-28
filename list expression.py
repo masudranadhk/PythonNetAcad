c9fe741a05caa8ab93f399b0ad4bbd835ab68e76
masudranadhk <masudranadhk.bank@gmail.com>
2021-04-29 02:43:10 +0600

    List

---
+++ list expression.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+listijk = []
+for i in range(x + 1):
+    for j in range(y + 1):
+        for k in range(z + 1):
+            if i + j + k != n:  # before printing the result, it will exclude the output which 'i' + 'j' + 'k' is the same as 'n'.
+                listijk.append([i, j, k])
+print(listijk)
